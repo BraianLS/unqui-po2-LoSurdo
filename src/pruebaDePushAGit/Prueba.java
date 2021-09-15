bfe0a8fe503bbfaee148c0995bc7d19f2a74cd8e
BraianLS <cosmepepe049@gmail.com>
2021-09-15 20:13:19 -0300

    Agregada clase para probar github

--- src/pruebaDePushAGit/Prueba.java
+++ src/pruebaDePushAGit/Prueba.java
@@ -5,7 +5,7 @@ public class Prueba {
 	public static void main(String[] args) {
 
 	}
-	
+// esto deberia poder pushearse
 	public void probarConsola() {
 		System.out.println("Probando 1 2 3 ");
 	}
