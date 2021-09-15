45838881fded2b369dcbd014c8304e84ec106f64
BraianLS <cosmepepe049@gmail.com>
2021-09-15 20:13:00 -0300

    Agregué una clase

---
+++ src/pruebaDePushAGit/Prueba.java
@@ -0,0 +1,13 @@
+package pruebaDePushAGit;
+
+public class Prueba {
+
+	public static void main(String[] args) {
+
+	}
+	
+	public void probarConsola() {
+		System.out.println("Probando 1 2 3 ");
+	}
+
+}
